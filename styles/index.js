7db7d987faf3a6578d077addbd62e72dca3c2c34
CalvinAndrean <75055586+CalvinAndrean@users.noreply.github.com>
2023-07-30 22:08:34 +0700

    styles file

---
+++ styles/index.js
@@ -0,0 +1,26 @@
+const styles = {
+  innerWidth: '2xl:max-w-[1280px] w-full',
+  interWidth: 'lg:w-[80%] w-[100%]',
+
+  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  yPaddings: 'sm:py-16 xs:py-8 py-12',
+  xPaddings: 'sm:px-16 px-6',
+  topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
+  bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+
+  flexCenter: 'flex justify-center items-center',
+  flexStart: 'flex justify-start items-start',
+  flexEnd: 'flex justify-end',
+  navPadding: 'pt-[98px]',
+
+  // hero section
+  heroHeading:
+      'font-bold mb-[72px] text-center lg:text-[108px] md:text-[85px] sm:text-[64px] text-[44px] lg:leading-[110.4px] md:leading-[80.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white lg:mb-[90px] md:mb-[110px] sm:mb-[50px]',
+
+    guwosariHeading: 
+    'font-bold mt-[-30px] mb-[30px] lg:text-[48px] md:text-[38px] sm:text-[27px] text-[24px] leading-[64.4px] uppercase text-white lg:mb-[10px] md:mb-[28px] sm:mb-[30px] lg:mt-[-80px] md:mt-[-50px] sm:mt-[-30px]',
+  heroDText:
+      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+};
+
+export default styles;
