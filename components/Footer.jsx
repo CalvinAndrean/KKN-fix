1594c79bb561b4a9601e15285d4c5ac8afc13e94
CalvinAndrean <75055586+CalvinAndrean@users.noreply.github.com>
2023-07-30 22:01:55 +0700

    components file

---
+++ components/Footer.jsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
+const Footer = () => (
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold sm:text-[32px] md:text-[64px] text-[32px] text-white">
+          Salam Kenal
+        </h4>
+        {/* <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="font-normal text-[16px] text-white">
+            Enter Metaverse
+          </span>
+        </button> */}
+      </div>
+
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold sm:text-[18px] text-[24px] text-white">
+            Pringgading
+          </h4>
+          <p className="font-normal text-[14px] text-white">
+            Made with 💖 by Calvin.
+          </p>
+
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <a href="https://instagram.com/kkn83.pringgading?igshid=MzRlODBiNWFlZA==">
+                <img
+                  key={social.name}
+                  src={social.url}
+                  alt={social.name}
+                  className="w-[24px] h-[24px] object-contain cursor-pointer"
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
+);
+
+export default Footer;
