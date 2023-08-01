444a6ca089a2357669ac3851e267c3c2c34a9572
CalvinAndrean <75055586+CalvinAndrean@users.noreply.github.com>
2023-08-01 07:49:01 +0700

    Update Day4.jsx

--- sections/Day4.jsx
+++ sections/Day4.jsx
@@ -9,25 +9,26 @@ export default function ActionAreaCard() {
   return ( 
     <div>
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <div href="#" className='max-w-[500px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/day4/prepare-sarakata.jpeg" alt="" />
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Persiapan acara karang taruna (SARAKATA)</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Hari keempat ini kami diminta untuk membantu persiapan acara SARAKATA yang bertempat di GOR Kalurahan Guwosari. Tiap-tiap kelompok pada 
+              Hari keempat ini kami diminta untuk membantu persiapan acara SARAKATA yang bertempat di GOR Kalurahan Guwosari. Tiap-tiap kelompok pada 
                 Unit B mengirimkan perwakilan sebanyak 4 orang untuk persiapan acara ini dan perwakilan kelompok 9 adalah Ruliff, Aldyo, Calvin, dan Nikko.</p>
           </div>
         </div>
       </div>
 
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <div href="#" className='max-w-[500px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/day4/sarakata.jpg" alt="" />
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Menghadiri acara SARAKATA</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                SARAKATA merupakan singkatan dari Sambung Rasa Karang Taruna dimana acara ini dihadiri oleh seluruh karang taruna se-Kabupaten Bantul dan 
-                partisipasi kehadiran dari mahasiswa KKN 83 UAJY pada Kapanewon Pajangan dan Kapanewon Sedayu masing masing kelompok adalah 4 orang.</p>
+              Hari ketiga kami dimulai dengan pelaksanaan program kerja Inovasi Dusun yakni Pemasangan Plang pada tiap-tiap rumah
+                Ketua RT.01 - RT.09. Mengingat motor yang boleh kami bawa hanya 3 saja, maka program kerja inovasi dusun pemasangan plang ini dilaksanakan oleh <span className="text-white"><b>'9 UnderGround's' </b></span>
+                yang merupakan sebutan untuk kumpulan lelaki di KKN 83 UAJY Pringgading.</p>
           </div>
         </div>
       </div>
