2fde0d25b16fa23f0860f3385f9f4ae128b0bd3c
CalvinAndrean <75055586+CalvinAndrean@users.noreply.github.com>
2023-08-01 07:48:25 +0700

    Update Day1.jsx

--- sections/Day1.jsx
+++ sections/Day1.jsx
@@ -9,7 +9,7 @@ export default function ActionAreaCard() {
   return ( 
     <div>
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <div href="#" className='max-w-[500px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/day1/KKN-UAJY.jpg" alt="" />
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Penerjunan KKN 83 UAJY Pringgading</h5>
@@ -22,12 +22,12 @@ export default function ActionAreaCard() {
       </div>
 
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <div href="#" className='max-w-[500px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/day1/pembukaan-kapanewon.jpg" alt="" />
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Penyambutan oleh Kapanewon Pajangan</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Sebelum memulai pelaksaan program kerja, kami disambut oleh Wakil Kapanewon Pajangan, Lurah, 
+              Sebelum memulai pelaksaan program kerja, kami disambut oleh Wakil Kapanewon Pajangan, Lurah, 
               dan Polsek Bantul disertai dengan himbauan-himbauan dalam melakukan kegiatan sehari-hari di padukuhan masing masing.</p>
           </div>
         </div>
