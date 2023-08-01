b4d56d044004a1435958c66b74b9788e246b61d2
CalvinAndrean <75055586+CalvinAndrean@users.noreply.github.com>
2023-08-01 07:49:35 +0700

    Update Day6.jsx

--- sections/Day6.jsx
+++ sections/Day6.jsx
@@ -9,23 +9,25 @@ export default function ActionAreaCard() {
   return ( 
     <div>
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <div href="#" className='max-w-[500px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
           <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/day6/env3.jpg" alt="" />
           <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kerja Bakti Merti Dusun</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Merti Dusun merupakan acara untuk memperingati hari ulang tahun dusun dimana selalu dilaksanakan setiap tahunnya.
+              Merti Dusun merupakan acara untuk memperingati hari ulang tahun dusun dimana selalu dilaksanakan setiap tahunnya.
                 Untuk mempersiapkan acara merti dusun, dilaksanakan kerja bakti setiap hari minggu bertempat di lahan belakang rumah Wakil Bupati Bantul.</p>
           </div>
         </div>
       </div>
 
       <div className='flex justify-center mt-[64px] mb-[64px]'>
-        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
-          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-64 md:rounded-none md:rounded-l-lg mr-[30px]" src="/day6/env1.jpg" alt="" />
-          <img class="object-cover w-full h-96 md:h-auto md:w-64 mr-[30px]" src="/day6/env2.jpg" alt="" />
-          <img class="object-cover w-full h-96 md:h-auto md:w-64 mr-[30px]" src="/day6/kerja-bakti.jpg" alt="" />
-          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-64 md:rounded-none md:rounded-r-lg" src="/day6/env4.jpg" alt="" />
+        <div href="#" className='max-w-[1200px] flex items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-4xl md:flex-row md:max-w-xl sm:flex-row sm:max-w-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+          <div class="flex flex-col p-4 leading-normal inline">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-64 md:rounded-none md:rounded-l-lg mr-[30px]" src="/day6/env1.jpg" alt="" />
+            <img class="object-cover w-full h-96 md:h-auto md:w-64 mr-[30px]" src="/day6/env2.jpg" alt="" />
+            <img class="object-cover w-full h-96 md:h-auto md:w-64 mr-[30px]" src="/day6/kerja-bakti.jpg" alt="" />
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-64 md:rounded-none md:rounded-r-lg" src="/day6/env4.jpg" alt="" />
+          </div>
           {/* <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kerja Bakti Merti Dusun</h5>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
